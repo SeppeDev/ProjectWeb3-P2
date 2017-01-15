93c579a67ff20e1efe2080cc1d0baad389c930f4
Robin De Herdt <robindh95@gmail.com>
2017-01-15 11:42:55 +0100

    Fixes

--- Web/app/services/instrumentService.js
+++ Web/app/services/instrumentService.js
@@ -18,4 +18,22 @@ app.service("instrumentService", function($http) {
 		
 		return getData(url);
 	}
+
+	// Initial values (will be overwritte by db values)
+	svc.instruments = [{
+ 							"name":"Lead-guitar",
+ 							"id":1
+ 						},
+ 						{
+ 							"name":"Drum",
+ 							"id":2
+ 						},
+ 						{
+ 							"name":"Bass",
+							"id":3
+ 						},
+						{
+							"name":"Keys",
+ 							"id":4
+ 						}];
 })
